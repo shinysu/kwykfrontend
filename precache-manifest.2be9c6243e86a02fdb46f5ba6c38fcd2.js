6a4931fec49a6b728c6484ce2ad53f34aa21aaf2
shinysu <shiny.suresh@gmail.com>
2020-08-22 11:19:55 +0530

    Updates

---
+++ precache-manifest.2be9c6243e86a02fdb46f5ba6c38fcd2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe1b75d8dcc8f1ef0be95de182e5775",
+    "url": "/kwykfrontend/index.html"
+  },
+  {
+    "revision": "fbadd0199bd803b45434",
+    "url": "/kwykfrontend/static/css/main.78a33340.chunk.css"
+  },
+  {
+    "revision": "a46707bd2779907edb5f",
+    "url": "/kwykfrontend/static/js/2.5942e09e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kwykfrontend/static/js/2.5942e09e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbadd0199bd803b45434",
+    "url": "/kwykfrontend/static/js/main.eccfe6e2.chunk.js"
+  },
+  {
+    "revision": "d9c4785ea1e9ed29cd20",
+    "url": "/kwykfrontend/static/js/runtime-main.c8876517.js"
+  },
+  {
+    "revision": "2dea87fd5d47090ecc87f51144a48e8e",
+    "url": "/kwykfrontend/static/media/avatar.2dea87fd.png"
+  },
+  {
+    "revision": "4ffec318c7a43fceb0542ae5766b55c0",
+    "url": "/kwykfrontend/static/media/clock.4ffec318.png"
+  },
+  {
+    "revision": "ae02fd801b69396a2e368d2d635f4c78",
+    "url": "/kwykfrontend/static/media/knowbotSVG.ae02fd80.svg"
+  },
+  {
+    "revision": "2f9b2beea54bb6ab9cc061cb57cfbac8",
+    "url": "/kwykfrontend/static/media/send.2f9b2bee.png"
+  }
+]);
